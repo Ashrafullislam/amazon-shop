3ae2d49d0351b9af14577b310f85e25c916a1d59
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-10-05 20:57:40 +0600

    added the local storage

--- src/component/AddToLS/AddToLS.js
+++ src/component/AddToLS/AddToLS.js
@@ -1,22 +1,29 @@
 
  import React from 'react';
- 
  const AddToLS = (id) => {
-  console.log(id)
-// first time declare a varible ,to reserved the value like as object
+ // first time declare a varible ,to reserved the value like as object
   let shoppingCart = {};
+
+ //get the shopping cart from LS and count 
+ const storedCart = localStorage.getItem('shoppingCart');
+ if(storedCart){
+  shoppingCart = JSON.parse(storedCart);
+ }
+ else{
+ shoppingCart = {}
+ }
+
   const quantity = shoppingCart[id];
-  if(quantity){
-    const newQuantity = quantity + 1;
+  if(quantity){ 
+       const newQuantity = quantity + 1;
     shoppingCart[id] = newQuantity;
   }
   else{
     shoppingCart[id] = 1;
   }
-
-
    localStorage.setItem('shoppingCart',JSON.stringify(shoppingCart))
 
  };
  
  export default AddToLS;
+
